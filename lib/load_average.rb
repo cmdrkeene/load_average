2ce604dddb94615035b60789cffa9264eb6d249f
Brandon Keene <bkeene@gmail.com>
2011-06-23 17:12:56 -0400

    Round to 2 places

--- lib/load_average.rb
+++ lib/load_average.rb
@@ -1,15 +1,16 @@
 module LoadAverage
-  FSHIFT  = 11
-  FIXED_1 = 1 << FSHIFT
-  EXP_1   = 1884.0 # 1/exp(5sec/1min) as fixed-point
-  EXP_5   = 2014.0 # 1/exp(5sec/5min)
-  EXP_15  = 2037.0 # 1/exp(5sec/15min)
-  $ticks  = 0
-  $load   = [0.0, 0.0, 0.0, 0.0] # sec, min, 5 min, 15 min
+  INTERVAL  = 10.0 # sec
+  FSHIFT    = 11
+  FIXED_1   = 1 << FSHIFT
+  EXP_1     = 1884.0 # 1/exp(5sec/1min) as fixed-point
+  EXP_5     = 2014.0 # 1/exp(5sec/5min)
+  EXP_15    = 2037.0 # 1/exp(5sec/15min)
+  $ticks    = 0
+  $load     = [0.0, 0.0, 0.0, 0.0] # sec, min, 5 min, 15 min
 
   class << self
-    def compute(interval)
-      per_second = $ticks / interval.to_f
+    def compute
+      per_second = $ticks / INTERVAL
       $load[0] = per_second
       $load[1] = average($load[1], EXP_1, per_second)
       $load[2] = average($load[2], EXP_5, per_second)
@@ -18,14 +19,26 @@ module LoadAverage
       $load
     end
 
+    def tick
+      $ticks += 1
+    end
+
+    private
+    
     def average(load, exp, n)
       load *= exp
       load += n*(FIXED_1-exp)
-      ((load * 1000).to_i >> FSHIFT) / 1000.0
+      load = (((load * 1000).to_i >> FSHIFT) / 1000.0)
+      round(load, 2)
     end
 
-    def tick
-      $ticks += 1
+    def round(float, precision = nil)
+      if precision
+        magnitude = 10.0 ** precision
+        (float * magnitude).round / magnitude
+      else
+        float.round
+      end
     end
   end
 end
